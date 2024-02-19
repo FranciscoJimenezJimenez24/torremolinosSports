43f46223b7929dfcc69460da3d57793a40e08128
FranciscoJimenezJimenez24 <146998590+FranciscoJimenezJimenez24@users.noreply.github.com>
2024-02-19 13:17:42 +0100

    update

--- urls.py
+++ urls.py
@@ -16,7 +16,27 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.inicio),
+
+    path('deportes/', views.DeportesListView.as_view(), name='deportes_lista'),
+    path('deportes/create', views.DeporteCreateView.as_view(), name='deportes_create'),
+    path('deportes/<int:pk>/update/', views.DeportesListView.as_view(), name='deportes_update'),
+    path('deportes/<int:pk>/delete/', views.DeportesListView.as_view(), name='deportes_delete'),
+
+    path('instalaciones/', views.InstalacionesListView.as_view(), name='instalaciones_lista'),
+    path('instalaciones/create', views.InstalacionCreateView.as_view(), name='instalaciones_create'),
+    path('instalaciones/<int:pk>/update/', views.InstalacionUpdateView.as_view(), name='instalaciones_update'),
+    path('instalaciones/<int:pk>/delete/', views.InstalacionDeleteView.as_view(), name='instalaciones_delete'),
+
+    path('equipos/', views.EquiposListView.as_view(), name='equipos_lista'),
+    path('equipos/create/', views.EquipoCreateView.as_view(), name='equipo_create'),
+    path('equipos/<int:pk>/', views.EquipoDetailView.as_view(), name='equipo_detalle'),
+    path('equipos/<int:pk>/update/', views.EquipoUpdateView.as_view(), name='equipo_update'),
+    path('equipos/<int:pk>/delete/', views.EquipoDeleteView.as_view(), name='equipo_delete'),
+    path('equipos/<int:pk>/jugadores/nuevo/', views.JugadorCreateView.as_view(), name='jugador_create'),
+
 ]
--- views.py
+++ views.py
@@ -1,6 +1,10 @@
 import datetime
 from django.shortcuts import render
 from .models import Partido
+from django.views import generic
+
+import models
+
 
 def inicio(request):
     # Obtener los últimos cinco partidos jugados
@@ -12,3 +16,92 @@ def inicio(request):
         fecha_hora__gte=datetime.now())[:5]
 
     return render(request, 'inicio.html', {'ultimos_partidos': ultimos_partidos, 'proximos_partidos': proximos_partidos})
+
+def deportes(request):
+    deportes=models.Deporte.objects.all()
+    contexto={}
+    contexto['deportes']=deportes
+    return render(request,"deportes.html",contexto)
+
+def instalaciones(request):
+    instalaciones=models.Instalacion.objects.all()
+    contexto={}
+    contexto['instalaciones']=instalaciones
+    return render(request,"instalaciones.html",contexto)
+
+class DeportesListView(generic.ListView):
+    model=models.Deporte
+    template_name = "deportes.html"
+
+class DeporteCreateView(generic.CreateView):
+    model = models.Deporte
+    fields = []
+    template_name = 'create.html'
+    success_url = "/inicio/deporte/"
+
+class DeporteUpdateView(generic.UpdateView):
+    model = models.Deporte
+    fields = []
+    template_name = 'update.html'
+    success_url = "/inicio/deporte/"
+
+class DeporteDeleteView(generic.DeleteView):
+    model = models.Deporte
+    fields = []
+    template_name = 'delete.html'
+    success_url = "/inicio/deporte/"
+
+class InstalacionesListView(generic.ListView):
+    model = models.Instalacion
+    template_name = 'instalaciones.html'
+
+class InstalacionCreateView(generic.CreateView):
+    model = models.Instalacion
+    template_name = 'create.html'
+    fields = []
+    success_url = "/inicio/instalacion/"
+
+class InstalacionUpdateView(generic.UpdateView):
+    model = models.Instalacion
+    template_name = 'update.html'
+    fields = []
+    success_url = "/inicio/instalacion/"
+
+class InstalacionDeleteView(generic.DeleteView):
+    model = models.Instalacion
+    template_name = 'delete.html'
+    fields = []
+    success_url = "/inicio/instalacion/"
+
+class EquiposListView(generic.ListView):
+    model = models.Equipo
+    template_name = 'equipos.html'
+    success_url = "/inicio/equipo/"
+
+class EquipoDetailView(generic.DetailView):
+    model = models.Equipo
+    template_name = 'equipo_detalle.html'
+    success_url = "/inicio/equipo/"
+
+class EquipoCreateView(generic.CreateView):
+    model = models.Equipo
+    fields = ['nombre', 'deporte', 'contacto', 'telefono', 'email']
+    template_name = 'create.html'
+    success_url = "/inicio/equipo/"
+
+class EquipoUpdateView(generic.UpdateView):
+    model = models.Equipo
+    fields = ['nombre', 'deporte', 'contacto', 'telefono', 'email']
+    template_name = 'update.html'
+    success_url = "/inicio/equipo/"
+
+class EquipoDeleteView(generic.DeleteView):
+    model = models.Equipo
+    template_name = 'delete.html'
+    success_url = "/inicio/equipo/"
+
+class JugadorCreateView(generic.CreateView):
+    model = models.Jugador
+    fields = ['nombre', 'apellido1', 'apellido2', 'dorsal', 'fecha_nacimiento', 'altura', 'peso', 'telefono']
+    template_name = 'create.html'
+    success_url = 'inicio/equipo/'  # Ajusta esta URL según tu estructura de URL
